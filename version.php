51e6e360b4c73b50356b0d6a9f82a880de813d31
sharon <sharon.wu@ossii.com.tw>
2021-04-08 15:59:40 +0800

    增加版號參數，避免禁止跨版號升級

--- version.php
+++ version.php
@@ -1,15 +1,16 @@
-<?php 
+<?php
 $OC_Version = array(20,0,8,1);
 $OC_VersionString = '20.0.8';
 $OC_Edition = '';
 $OC_Channel = 'stable';
 $OC_VersionCanBeUpgradedFrom = array (
-  'nextcloud' => 
+  'nextcloud' =>
   array (
+    '16.0' => true,
     '19.0' => true,
     '20.0' => true,
   ),
-  'owncloud' => 
+  'owncloud' =>
   array (
     '10.5' => true,
   ),
