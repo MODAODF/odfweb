2bc60df72ecf56efdaf4450aa4044c44cee8686b
sharon <sharon.wu@ossii.com.tw>
2021-10-14 16:34:37 +0800

    更新 mergeodf-2.0.1

--- apps/mergeodf/templates/appnavigation.php
+++ apps/mergeodf/templates/appnavigation.php
@@ -4,20 +4,17 @@
 		<?php
 
 		$pinned = 0;
-		foreach ($_['navigationItems'] as $item) {
-			$pinned = NavigationListElements($item, $l, $pinned);
+		if(count($_['navigationItems']) == 0){
+			print("<li> 目前沒有範本資料夾 </li>");
+		} else {
+			foreach ($_['navigationItems'] as $item) {
+				$pinned = NavigationListElements($item, $l, $pinned);
+			}
 		}
+
 		?>
 
 	</ul>
-	<div id="app-settings">
-		<div id="app-settings-header">
-			<button class="settings-button"
-					data-apps-slide-toggle="#app-settings-content">
-				<?php p($l->t('Settings')); ?>
-			</button>
-		</div>
-	</div>
 </div>
 
 
